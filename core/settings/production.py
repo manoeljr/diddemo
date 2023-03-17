7068dcb9e379c0623913634d94f2f1fe8121e869
manoeljr <manoelv8@gmail.com>
2023-03-16 22:36:57 -0300

    Created project inicial

---
+++ core/settings/production.py
@@ -0,0 +1,11 @@
+from core.settings.base import *
+
+ALLOWED_HOSTS = ['*']
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / '../db.sqlite3',
+    }
+}
